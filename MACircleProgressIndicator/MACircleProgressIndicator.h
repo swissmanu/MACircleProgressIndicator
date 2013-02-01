92ecc2cf74468594ced3075c1493fe93606eea7f
Manuel Alabor <msites@msites.net>
2013-02-01 20:38:31 +0100

    added comments to header file

--- MACircleProgressIndicator/MACircleProgressIndicator.h
+++ MACircleProgressIndicator/MACircleProgressIndicator.h
@@ -2,10 +2,32 @@
 
 @interface MACircleProgressIndicator : UIView
 
+/**
+ * Represents the displayed progress value. Set it to update the progress indicator.
+ * Pass a float number between 0.0 and 1.0.
+ */
 @property (nonatomic) float value;
-@property (nonatomic, retain) UIColor *color UI_APPEARANCE_SELECTOR;
-@property (nonatomic) CGFloat strokeWidthRatio UI_APPEARANCE_SELECTOR;
-@property (nonatomic) CGFloat strokeWidth UI_APPEARANCE_SELECTOR;
 
+/**
+ * The color which is used to draw the progress indicator. Use UIAppearance to
+ * style according your needs.
+ */
+@property (nonatomic, retain) UIColor *color UI_APPEARANCE_SELECTOR;
+
+/**
+ * The stroke width ratio is used to calculate the circle thickness regarding the
+ * actual size of the progress indicator view. When setting this, strokeWidth is
+ * ignored.
+ * Use UIAppearance to style according your needs.
+ */
+@property (nonatomic) CGFloat strokeWidthRatio UI_APPEARANCE_SELECTOR;
+
+/**
+ * If you'd like to specify the stroke thickness of the progress indicator circle
+ * explicitly, use the strokeWidth property. When setting this, strokeWidthRatio
+ * is ignored.
+ * Use UIAppearance to style according your needs.
+ */
+@property (nonatomic) CGFloat strokeWidth UI_APPEARANCE_SELECTOR;
 
 @end
